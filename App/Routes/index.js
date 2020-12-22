66e26ca131aeba81f48939695f0c002645807aaf
Jessé Orestes <jesseorestespereira1@gmail.com>
2020-12-21 21:45:13 -0300

    correção status

--- App/Routes/index.js
+++ App/Routes/index.js
@@ -1,6 +1,6 @@
-module.exports = (App) =>{
+module.exports = (App) => {
     App.use('/api', require('./Users'));
     App.use(function(req, res) {
-        res.status(400).send({Error: `Error 404: Path(${req.url}) not found for method(${req.method})`});
+        res.status(404).send({Error: `Error 404: Path(${req.url}) not found for method(${req.method})`});
     })
 }
